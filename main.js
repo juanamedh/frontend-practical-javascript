7a79ebab0fcb11e4788474af32a342cce6f9d64a
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-12 18:37:55 -0700

    toggle shopping cart w/js code

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamburgerMenu = document.querySelector('nav img.menu');
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const cartMenuIcon = document.querySelector('li.navbar-shopping-cart');
+const shoppingCartMenu = document.querySelector('aside.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamburgerMenu.addEventListener('click', toggleMobileMenu);
+cartMenuIcon.addEventListener('click', toggleShoppingCartMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+function toggleShoppingCartMenu() {
+    shoppingCartMenu.classList.toggle('inactive');
 }
