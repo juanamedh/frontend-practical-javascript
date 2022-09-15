d12a87c5a44e09449c10ba0d0821eb9741682711
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-15 12:56:08 -0700

    fix ; typos

--- main.js
+++ main.js
@@ -44,11 +44,11 @@ productList.push({
 productList.push({
     name: 'LCD TV',
     price: 220,
-    image; 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
     name: 'Computer',
     price: 620,
-    image; 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
